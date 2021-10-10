f24cd3cc9f0ba5898b51b7f7bfa872fb878dfa51
Valerie Hernández <67847926+valeriehernandez-7@users.noreply.github.com>
2021-10-10 14:12:21 -0600

    Project Structure
    NOTE: empty.txt files are temp

---
+++ Image-Recovery/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Image-Recovery" << endl;
+    return EXIT_SUCCESS;
+}
---
+++ Rent-A-Car/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Rent-A-Car" << endl;
+    return EXIT_SUCCESS;
+}
